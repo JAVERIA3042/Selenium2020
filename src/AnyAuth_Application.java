ce3f547a52671be81570de8b9ec562c300a5ed6c
javeria <javeria.khaliq3042@gmail.com>
2020-04-22 12:49:01 -0400

    AnyAuth_Application

---
+++ src/AnyAuth_Application.java
@@ -0,0 +1,8 @@
+
+public class AnyAuth_Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/Basics.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basics {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("My first program");
+
+		System.setProperty("webdriver.chrome.driver", "C://Users//Public//Selenium//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("email")).sendKeys("everJaveria@hotmail.com");
+		driver.findElement(By.id("pass")).sendKeys(("22802280Javeria"));
+		driver.findElement(By.linkText("Forgot account?")).click();
+		// driver.findElement(By.XPtah()).click();
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+}
